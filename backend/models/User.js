e913e43775bbe2abd3a38f921851c28c7f1fa852
sagardhondge <sagardhondge56@gmail.com>
2025-09-05 23:07:47 +0530

    Backend

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import moongoose from 'mongoose';
+
+const userSchema = new moongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const User = moongoose.model('User', userSchema);
+export default User;
